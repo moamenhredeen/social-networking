070b02918395cf5d5e26f8d523accdcadc0c90df
moamnehraden <momenhredien@gmail.com>
2020-10-01 00:17:17 +0200

    change navbar, create post-input

--- Frontend/script/index.js
+++ Frontend/script/index.js
@@ -1,1 +1,27 @@
-console.log("test")
+
+
+$(document).ready(() =>  {
+
+    // global variables : 
+    const asideNavLinks = $('#aside-nav-links');
+    const btn_burger = $('#burger');
+    const btn_close = $('#close');
+
+    // initial state 
+    asideNavLinks.hide();
+
+
+    // aside nav bar :show / hide
+    btn_burger.click(() => {
+        btn_burger.hide();
+        btn_close.show();
+        asideNavLinks.show();
+    })
+
+    btn_close.click(() => {
+        btn_close.hide();
+        btn_burger.show();
+        asideNavLinks.hide();
+    })
+
+});
